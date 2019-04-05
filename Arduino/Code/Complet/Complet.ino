49c74c2180360a87c7f01e4f8998a850e7cdee6f
webgburnet <gregoire.burnet@laposte.net>
2019-04-05 09:07:59 +0200

    Update Complet.ino

--- Arduino/Code/Complet/Complet.ino
+++ Arduino/Code/Complet/Complet.ino
@@ -1,31 +1,33 @@
 #include  "Seeed_BMP280.h"// bibliothèque du capteur de Pression
 #include  <Wire.h> // bibliothèque du capteur de Pression
-
-BMP280  bmp280 ; // nom du capteur 
-
 #include <Keypad.h>//bibliothèque du keypad
-
 #include <math.h> // bibliothèque pour les fonctions mathématiques
+#include "Seeed_MCP9808.h"//bibliothèque du capteur de température
+#include <Hx711.h> // bibliothèque du capteur de force
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
 
-#include "Seeed_MCP9808.h"//bibliothèque du capteur de température 
-MCP9808  sensor; // nom du capteur 
+// For the Adafruit shield, these are the default.
+#define TFT_DC 9
+#define TFT_CS 10
+#define  magnet  6 // la variable magnet est de type integer
+#define  DOUT  A8
+#define  DSCK  A9
+#define  Keypad1  6
+#define  Keypad2  7
+#define  Keypad3  8
+#define  Keypad4  9
+#define  Keypad5  11
+#define  Keypad6  12
+#define  Keypad7  13
 
-float Force=0; // Force est déclaré comme étant un float
-#include <Hx711.h> // bibliothèque du capteur de force  
-Hx711 scale(A8, A9); // la capteur de force se branche en analogique sur les ports 2 et 3 
+  Hx711 scale(DOUT, DSCK); // la capteur de force se branche en analogique sur les ports 2 et 3 
+  Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC); // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
+  BMP280  bmp280 ; // nom du capteur 
+  MCP9808  sensor; // nom du capteur 
 
-float const R=8.3144621; // R est déclaré comme étant une variable constante de type float
-float const M_air=0.02896; // M est déclaré comme étant une variable constante de type float
-float const K=273.15;
 
-unsigned long temps; // la variable temps est de typoe long
-unsigned long start ;
-
-int a = 0; // la variable a est de type integer
-int n = 0; // la variable n est de type integer
-#define  magnet  6 // la variable magnet est de type integer 
-float v = 0; // la variable v est de type integer 
-int S =0;
 
 const byte ROWS = 4; //4 lignes
 const byte COLS = 3; //3 colones
@@ -35,8 +37,8 @@ char keys[ROWS][COLS] = {   // description de la matrice
 {'7','8','9'},
 {'*','0','#'}
 };
-byte rowPins[ROWS] = {11, 9, 7, 6};   //definiton des broches pour les lignes
-byte colPins[COLS] = {8, 13, 12};     //definiton des broches pour les colones
+byte rowPins[ROWS] = {Keypad5, Keypad4, Keypad2, Keypad1};   //definiton des broches pour les lignes
+byte colPins[COLS] = {Keypad3, Keypad7, Keypad6};     //definiton des broches pour les colones
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); //initialisation de la matrice
 
 int entree_nombre=0; //la variable entree_nombre est de type integer
@@ -46,21 +48,23 @@ int j=0; // la variable j est de type integer
 int k=0; //la variable k est de type integer
 int Surface=0; // la variable Surface est de type integer
 
-float Cx; // la variable Cx est de type float
+float Cx=0; // la variable Cx est de type float
 float Fx=0; // la variable Fx est de type float
-int Cx_flot; // la variable Cx_flot est de type integer
 
-//Ecran TFT
-#include "SPI.h"
-#include "Adafruit_GFX.h"
-#include "Adafruit_ILI9341.h"
+unsigned long temps=0; // la variable temps est de typoe long
+unsigned long start=0 ;
 
-  // For the Adafruit shield, these are the default.
-  #define TFT_DC 9
-  #define TFT_CS 10
+int a = 0; // la variable a est de type integer
+int n = 0; // la variable n est de type integer
+float v = 0; // la variable v est de type integer 
+int S =0;
 
-  // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
-  Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+float Force=0; // Force est déclaré comme étant un float
+
+
+float const R=8.3144621; // R est déclaré comme étant une variable constante de type float
+float const M_air=0.02896; // M est déclaré comme étant une variable constante de type float
+float const K=273.15;
 
   void setup() {
   // put your setup code here, to run once:
@@ -87,18 +91,13 @@ void loop() {
 
     float temp; // on déclare temp en tant que float 
     float temp_K; // on déclare temp_K en tant que float
-    int temp_dec = temp; // on déclare temp dec comme étant la valeur temp entière 
     sensor.get_temp(&temp);  //Obtenir la temperature, une valeur de type float
     temp_K=temp + K; // on convertie la valeur de la température en Kelvin
 
    float rho = (pressure*M_air)/(R*temp_K); // La valeur de rho est de type float et est égale à la pression multipliée par la masse molaire de l'air divisé par la constante R qui est multipliée par la température 
-//   int rho_dec=rho; // rho_dec représente la partie entiére de la valeur de rho
-//   int rho_flot=rho*100-rho_dec*100; // rho flot représente la valeur décimale de rho 
-   
+ 
    Force = scale.getGram(); //scale.getValue();//scale.getGram();//, 1; //utilisation de la fonction scale.getGram() pour obtenir la valeur de la masse
    Force = Force*0.00981; // La Force est égale a la force multipliée par 0.00981 
-//   int Force_dec=Force; // on déclare Force_dec comme étant la valeur entière 
-//   int Force_flot=Force*100-Force_dec*100; // on déclare Force_flot comme étant la valeur décimale 
    Fx = Force; // on dit que Fx est égale a la force
   
 
